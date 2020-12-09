335ec07fece969d7d43d91dcd13779cdfcd3fcb9
Daryna Horobei <daryna_horobei@epam.com>
2020-12-10 01:30:00 +0200

    commit fixed Home Work Regex

--- src/test/java/old/RegexMatcher.java
+++ src/test/java/old/RegexMatcher.java
@@ -1,13 +1,9 @@
 package old;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,7 +12,7 @@ import static driver.SingletonDriver.getDriver;
 
 public class RegexMatcher {
 
-    private final By addressEpam = By.xpath("//span[contains(text(),'41')");
+    private final By addressEpam = By.xpath("//b//span[@class='font-size-22']");
 
 
     //Task1
@@ -28,7 +24,7 @@ public class RegexMatcher {
             if (m.matches()) {
                 result.add(value);
                 System.out.println(value + " - Value is valid!");
-            } else System.out.println("Invalid");
+            } else continue;
         }
         return result;
     }
@@ -36,23 +32,19 @@ public class RegexMatcher {
 
     //Task5
 
-    String addressInfo = "41 University Drive • Suite 202,\n" +
-            "Newtown, PA 18940 • USA";
-
     public void getAndPrintEpamAddressElements() {
         getDriver().get("https://www.epam.com/about/who-we-are/contact");
         String epamAddress = getDriver().findElement(addressEpam).getText();
+        getDriver().quit();
+        String universalPattern = "^(\\d+)\\s(.+)•\\s(.+),\n(\\w+),\\s(.+)•\\s(\\w+)$";
 
-        String country = getValueByRegEx(epamAddress, "^(.+)•(.+)\\n(.+)• ([A-Z])$", 4);
+        String country = getValueByRegEx(epamAddress, universalPattern, 6);
         String streetNumber = getValueByRegEx(epamAddress, "^[0-9]{2}");
-        String streetName = getValueByRegEx(epamAddress, "^([0-9]{2})\\s(\\w| ) •(.+)$", 2);
-        String postCode = getValueByRegEx(epamAddress, "^(.+)\\n(\\w), ([A-Z]{2})\\s([0-9]{5}) •(.+)$",
-                4);
-        String suiteNumber = getValueByRegEx(epamAddress, "(.+)• (.+)\\s([0-9]{3}), \\n(.+)$", 3);
-        String city = getValueByRegEx(epamAddress, "(.+)\\n(\\w),(.+)$", 2);
+        String streetName = getValueByRegEx(epamAddress, universalPattern, 2);
+        String postCode = getValueByRegEx(epamAddress, universalPattern, 3);
+        String city = getValueByRegEx(epamAddress, universalPattern, 4);
 
-        System.out.println(country + "\n" + streetNumber + "\n" + streetName + "\n" + postCode + "\n" + suiteNumber +
-                "\n" + city);
+        System.out.println(country + "\n" + streetNumber + "\n" + streetName + "\n" + postCode + "\n" + city);
     }
 
 
@@ -72,9 +64,11 @@ public class RegexMatcher {
     //Task2,3
     public static void main(String[] args) {
         List<String> listOfValidAndInvalidEmails = new ArrayList<>();
-        listOfValidAndInvalidEmails.add("darynahorobei@ukr.net");
-        listOfValidAndInvalidEmails.add("daryna_horobei8ukr.net");
+        listOfValidAndInvalidEmails.add("daryna_horobei@ukr.net");
+        listOfValidAndInvalidEmails.add("daryna.horobei@ukr.net");
         listOfValidAndInvalidEmails.add("daryna_horobei@ukr");
+        listOfValidAndInvalidEmails.add("darynahorobei8ukr");
+
 
         List<String> listOfValidAndInvalidIP = new ArrayList<>();
         listOfValidAndInvalidIP.add("255.255.255.255");
@@ -82,16 +76,16 @@ public class RegexMatcher {
         listOfValidAndInvalidIP.add("33.3.3");
 
         List<String> listOfValidAndInvalidBankCard = new ArrayList<>();
-        listOfValidAndInvalidBankCard.add("4111222233334443");
+        listOfValidAndInvalidBankCard.add("4111 2222 3333 4443");
+        listOfValidAndInvalidBankCard.add("5111222233334443");
         listOfValidAndInvalidBankCard.add("jhhkjk566");
         listOfValidAndInvalidBankCard.add("54545645445554545444454j");
 
 
         RegexMatcher regexMatcher = new RegexMatcher();
-        regexMatcher.regexFiltering(listOfValidAndInvalidEmails, "[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z0-9]+");
-        regexMatcher.regexFiltering(listOfValidAndInvalidIP, "^(([1-9]?\\d|1\\d\\d|2[0-4]\\d|25[0-5])" +
-                "(\\.(?!$)|(?=$))){4}$");
-        regexMatcher.regexFiltering(listOfValidAndInvalidBankCard, "4[0-9]{12}(?:[0-9]{3})");
+        regexMatcher.regexFiltering(listOfValidAndInvalidEmails, "(^\\w+.?_?\\w+)@(\\w+[.]\\w+{2,})");
+        regexMatcher.regexFiltering(listOfValidAndInvalidIP, "([0-9]{1,3}[.]){3}([0-9]{1,3}){1}");
+        regexMatcher.regexFiltering(listOfValidAndInvalidBankCard, "((^[4|5]){1}([0-9]{3}) ?)([0-9]{4} ?){3}");
 
 
         regexMatcher.getAndPrintEpamAddressElements();
